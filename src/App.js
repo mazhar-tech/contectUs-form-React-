b52eb35c6a7447a74c0e12f16fd73e6d9b1d2a63
Muhammad Mazhar Iqbal <79931443+mazhariqbal-creater@users.noreply.github.com>
2023-02-01 13:34:32 +0500

    contectUs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import BasicExample from './Components/Form';
+import Login from './Components/Login';
+function App() {
+  return (
+    <div>
+    {/* <h1 className='text-center mt-6'>Client</h1> */}
+    {/* <BasicExample/> */}
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
